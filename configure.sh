4352ead0458426f26e9cd225f17e7e9eda260a0b
Igor Leonovich <8408481+igorleonovich@users.noreply.github.com>
2023-09-23 05:31:18 +0400

    Cleanup

--- configure.sh
+++ configure.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-yes "y" | ssh-keygen -o -a 100 -t ed25519 -C "alpine-sample" -f ./Private/key -N ""
+yes "y" | ssh-keygen -o -a 100 -t ed25519 -C "router-satellite-alpine" -f ./Private/key -N ""
